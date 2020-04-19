18e0fd3bc311fa1d40c9fa43d1bff1381e2e8c62
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-04-20 01:34:22 +0200

    Moved buttons array

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -2,11 +2,44 @@
 
 import NavButton from "./NavButton";
 
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faCompass,
+    faMapMarkerAlt,
+    faUser,
+    faProjectDiagram,
+
+} from "@fortawesome/free-solid-svg-icons";
+
+
+// Create list of Navbutton objects to be imported by the Navbuttons object
+const navButtons = [
+    {
+        label: "About",
+        path: "/About",
+        icon: <FontAwesomeIcon icon={faCompass} />
+    },
+    {
+        label: "Projects",
+        path: "/Projects",
+        icon: <FontAwesomeIcon icon={faProjectDiagram} />
+    },
+    {
+        label: "Contact",
+        path: "/Contact",
+        icon: <FontAwesomeIcon icon={faUser} />
+    }
+
+];
+
+
 // export navbar component 
 // props mapped to populate the component
 const NavBar = props => (
     <div className="NavBar">
-        {props.navButtons.map(button => (
+        {navButtons.map(button => (
             <NavButton
                 key={button.path}
                 path={button.path}
